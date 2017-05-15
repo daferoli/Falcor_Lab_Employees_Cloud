c0dbd2757897e89226736de8b71f11b56ae83577
David Feroli <daferoli@gmail.com>
2017-05-15 15:14:30 -0400

    made backend connections and added employee route

--- application.js
+++ application.js
@@ -1,23 +1,13 @@
 'use strict';
 
 const falcorExpress = require('falcor-express');
-const Router = require('falcor-router');
 
 const express = require('express');
 const app = express();
+const FalcorRoutes = require('lib/routes');
 
-app.use('/model.json', falcorExpress.dataSourceRoute(function (req, res) {
-  // create a Virtual JSON resource with single key ("greeting")
-  return new Router([
-    {
-      // match a request for the key "greeting"
-      route: 'greeting',
-      // respond with a PathValue with the value of "Hello World."
-      get: function() {
-        return {path:['greeting'], value: 'Hello World'};
-      }
-    }
-  ]);
+app.use('/model.json', falcorExpress.dataSourceRoute(function () {
+  return new FalcorRoutes();
 }));
 
 // serve static files from current directory
